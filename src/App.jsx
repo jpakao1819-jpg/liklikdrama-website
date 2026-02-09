32355f4e7cf0c6efeba22a352107dd232394cb71
jpakao1819-jpg <jpakao1819@gmail.com>
2026-02-10 08:29:26 +1000

    Combine App.css and index.css - unified Valentine's theme styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import './App.css';
 
 // Simple Carousel Component
 function Carousel() {
